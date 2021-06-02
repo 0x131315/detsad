1a110ea1a20bdc511b633a458a997947e6d007c3
tomas <tomas131315@gmail.com>
2021-06-02 16:44:44 +0300

    add KindGroup on api

--- app/src/Entity/KindGroup.php
+++ app/src/Entity/KindGroup.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\GroupRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -11,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass=GroupRepository::class)
  * @ORM\Table(name="`kind_group`")
  */
+#[ApiResource(collectionOperations: ['get'], itemOperations: ['get'])]
 class KindGroup
 {
     /**

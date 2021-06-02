6e6a0db09557a53a40c5fae6d422dfde20ee9d48
tomas <tomas131315@gmail.com>
2021-06-02 13:38:00 +0300

    user fixtures

--- app/src/DataFixtures/UserFixtures.php
+++ app/src/DataFixtures/UserFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
@@ -15,20 +16,31 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User();
-        $user
-            ->setEmail('admin@admin.com')
-            ->setRoles(['ROLE_USER', 'ROLE_ADMIN'])
-        ;
+        $faker = Factory::create('ru_RU');
+        $totalUsers = $faker->numberBetween(10, 100);
 
-        $user->setPassword(
-            $this->passwordEncoder->encodePassword(
-                $user,
-                'admin'// пароль
-            )
-        );
+        $manager->persist($this->buildUser('admin@admin.com', 'admin', 'ROLE_ADMIN'));
+        while ($totalUsers-- > 0) {
+            $email = $faker->unique()->safeEmail();
+            $role = $faker->optional(0.1, 'ROLE_TEACHER')->passthrough('ROLE_MANAGER');
+            $manager->persist($this->buildUser($email, 'user', $role));
+        }
 
-        $manager->persist($user);
         $manager->flush();
+    }
+
+    protected function buildUser(string $email, string $password, $roles): User
+    {
+        $user = new User();
+        $user->setRoles(array_unique(array_merge(['ROLE_USER'], (array)$roles)));
+        $user->setEmail($email);
+        $this->setPassword($user, $password);
+        return $user;
+    }
+
+    protected function setPassword(User $user, string $password)
+    {
+        $encoded = $this->passwordEncoder->encodePassword($user, $password);
+        $user->setPassword($encoded);
     }
 }
